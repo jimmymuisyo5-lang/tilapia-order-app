f854772cb94b0a1cd1bf4991acc7e3d356a22cdd
jimmymuisyo5-lang <jimmymuisyo5@gmail.com>
2026-08-13 11:13:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Path to data file
