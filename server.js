3359f2c3b5d09b5a284e1d1f2612713b4dbb800d
jimmymuisyo5-lang <jimmymuisyo5@gmail.com>
2026-08-14 17:48:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ============================================================
 
 // ⚠️ REPLACE WITH YOUR ACTUAL SUPABASE CREDENTIALS
-const SUPABASE_URL = 'https://jimmylanguser.supabase.co';
+const SUPABASE_URL = 'https://jimmylanguser1.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppbW15bGFuZ3VzZXIiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTczNDI0MjU4NSwiZXhwIjoyMDQ5ODE4NTg1fQ.YOUR_ACTUAL_KEY';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
